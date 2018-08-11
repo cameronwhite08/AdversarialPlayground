9d7edae8eab588480355f5eacf1071a90e235322
Cameron White <cameronwhite08@gmail.com>
2018-08-11 09:24:31 -0400

    renamed head directory

---
+++ MiniMax/MiniMax_TicTacToe_Lib/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MiniMax_TicTacToe_Lib
+{
+    public class Node
+    {
+        public readonly PieceType UpdatedPiece;
+
+        public int Value;
+
+        public readonly PieceType[][] Board;
+        public List<Node> Children;
+
+        public Node()
+        {
+            Value = -1;
+            Children = new List<Node>();
+        }
+
+        public Node(PieceType[][] _board, PieceType _piece) : this()
+        {
+            Board = _board;
+            UpdatedPiece = _piece;
+        }
+    }
+}
